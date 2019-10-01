f5b63029f3a0dc233d2137004d369a9d1a01bddc
Patrick Haney <patrick@hanerino.com>
2019-10-01 15:52:53 -0400

    Initial Gulp setup
    Both package.json and gulpfile are created, but do nothing

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require( 'gulp' ),
+    sass = require( 'gulp-sass' ),
+    csso = require( 'gulp-csso' ),
+    del = require( 'del' ),
+    autoprefixer = require( 'gulp-autoprefixer' );
